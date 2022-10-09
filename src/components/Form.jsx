5251b56d322942932a4df0b91dc5954c3d965670
ANDRES DIAZ <adiaz14@live.com>
2022-10-09 18:11:17 -0500

    Create list and section to list and add dragons

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,18 +11,45 @@ const Form = () => {
     <div className="container mt-5">
       <h1 className="text-center">BASIC CRUD</h1>
       <hr />
-      <form>
-        <input className="form-control md-2" type="text" placeholder="Dragon" />
-        <input
-          className="form-control md-2"
-          type="text"
-          placeholder="Description"
-        />
-        <input className="form-control md-2" type="text" placeholder="Age" />
-        <input className="form-control md-2" type="text" placeholder="Size" />
-        <input className="form-control md-2" type="text" placeholder="Color" />
-        <button className="btn btn-primary btn-block">Add new Dragon</button>
-      </form>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">List of dragons</h4>
+          <ul className="list-group"></ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center"></h4>
+          <form>
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Dragon"
+            />
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Description"
+            />
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Age"
+            />
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Size"
+            />
+            <input
+              className="form-control mb-2"
+              type="text"
+              placeholder="Color"
+            />
+            <button className="btn btn-primary btn-block">
+              Add new Dragon
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
