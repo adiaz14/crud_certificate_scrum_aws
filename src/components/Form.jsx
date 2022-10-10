c2300ac2e6ff9c0dc4f53f9cab83a6aa5d17364b
ANDRES DIAZ <adiaz14@live.com>
2022-10-09 23:27:54 -0500

    Add validations to the fields to store information

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,6 +9,7 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { useEffect } from "react";
+import swal from "sweetalert";
 
 const Form = () => {
   //States
@@ -27,11 +28,47 @@ const Form = () => {
     });
   }, []);
 
+  const showAlert = (title, field, status) => {
+    swal({
+      title: title,
+      text: field,
+      icon: status,
+      buttons: "Acept",
+    });
+  };
+
   //Methods
   //Save dragons
   const saveDragons = async (e) => {
     e.preventDefault();
+
     try {
+      //Validations
+      if (!dragonName.trim()) {
+        const field = "Please insert dragon name";
+        showAlert("Complete all the fields!", field, "error");
+        return;
+      }
+      if (!description.trim()) {
+        const field = "Please insert description about the dragon";
+        showAlert("Complete all the fields!", field, "error");
+        return;
+      }
+      if (!age.trim()) {
+        const field = "Please insert age of the dragon";
+        showAlert("Complete all the fields!", field, "error");
+        return;
+      }
+      if (!size.trim()) {
+        const field = "Please insert size of the dragon";
+        showAlert("Complete all the fields!", field, "error");
+        return;
+      }
+      if (!color.trim()) {
+        const field = "Please insert color of the dragon";
+        showAlert("Complete all the fields!", field, "error");
+        return;
+      }
       const data = await addDoc(collection(db, "dragons"), {
         dragonName: dragonName,
         description: description,
@@ -56,6 +93,11 @@ const Form = () => {
       setSize("");
       setColor("");
       e.target.reset();
+      showAlert(
+        "Saved data!",
+        "Your dragon has been successfully saved!",
+        "success"
+      );
     } catch (error) {
       console.log(error);
     }
@@ -81,6 +123,11 @@ const Form = () => {
     //console.log(id);
     try {
       await deleteDoc(doc(db, "dragons", id));
+      showAlert(
+        "Deleted data!",
+        "Your dragon has been successfully deleted!",
+        "success"
+      );
     } catch (error) {
       console.log(error);
     }
